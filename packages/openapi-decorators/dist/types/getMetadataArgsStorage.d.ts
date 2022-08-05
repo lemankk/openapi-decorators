200c5d5a4c6c8b3a3eb17168a9742a55b31f281b
LemanK <leman@lmswork.com>
2022-08-05 22:12:12 +0800

    chore: adding global setting

--- packages/openapi-decorators/dist/types/getMetadataArgsStorage.d.ts
+++
@@ -1,6 +0,0 @@
-import { MetadataStorage } from './metadata-builder/MetadataStorage';
-/**
- * Gets metadata args storage.
- * Metadata args storage follows the best practices and stores metadata in a global variable.
- */
-export declare function getMetadataArgsStorage(): MetadataStorage;
